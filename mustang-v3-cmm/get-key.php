ca33b21d670cab53cb535e7bee80971219dd565c
Colby <cmcdevitt21@yahoo.com>
2018-06-28 15:55:44 -0500

    update mustang v3 for muex lock

--- mustang-v3-cmm/get-key.php
+++ mustang-v3-cmm/get-key.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 $checkKey = fopen("key.json", "r") or die("Unable to open file to read!");
-$key = fread($checkKey,filesize("key.txt"));
+$key = fread($checkKey,filesize("key.json"));
 fclose($checkKey);
 echo $key;
 ?>
